b09d76e2100d2c0c92e5533efbf4e0aab58d0d42
Travless <travis.white0101@gmail.com>
2023-02-03 13:07:40 -0500

    Create createBoard function that will generate game boards for both player and comp via DOM manipulation

--- src/index.js
+++ src/index.js
@@ -1,100 +1,33 @@
-import _ from 'lodash';
+import _, { lowerFirst } from 'lodash';
 import './style.css';
 import { createGameboard, createSpace, createShip } from './classes';
 
-const content = document.getElementById('content');
+const flipButton = document.getElementById('flip-button');
+const startButton = document.getElementById('start-button');
+const optionsCont = document.querySelector('.options-cont');
+const gameboardsCont = document.querySelector('#gameboard-cont');
 
-
-
-
-function generatePage(){
-    const pageCont = document.createElement('div');
-    pageCont.classList.add('page-cont');
-    content.append(pageCont);
-
-    const turnCont = document.createElement('div');
-    turnCont.classList.add('turn-cont');
-    pageCont.append(turnCont);
-
-    const turnSpan = document.createElement('span');
-    turnSpan.classList.add('turn-span');
-    turnSpan.innerText = 'Turn: ';
-    turnCont.append(turnSpan);
-
-    const infoCont = document.createElement('div');
-    infoCont.classList.add('info-cont');
-    pageCont.append(infoCont);
-
-    const infoSpan = document.createElement('span');
-    infoSpan.classList.add('info-span');
-    infoSpan.innerText = 'Info: ';
-    infoCont.append(infoSpan);
-
-    // let gameboard = generateBoard();
-    const gameboard = document.createElement('div');
-    gameboard.classList.add('gameboard');
-    pageCont.append(gameboard);
-
-    const optionsCont = document.createElement('div');
-    optionsCont.classList.add('options-cont');
-    pageCont.append(optionsCont);
-
-    const carrierPreview = document.createElement('div');
-    carrierPreview.classList.add('carrier-preview');
-    carrierPreview.classList.add('carrier');
-    optionsCont.append(carrierPreview);
-
-    const battleshipPreview = document.createElement('div');
-    battleshipPreview.classList.add('battleship-preview');
-    battleshipPreview.classList.add('battleship');
-    optionsCont.append(battleshipPreview);
-
-    const cruiserPreview = document.createElement('div');
-    cruiserPreview.classList.add('cruiser-preview');
-    cruiserPreview.classList.add('cruiser');
-    optionsCont.append(cruiserPreview);
-
-    const submarinePreview = document.createElement('div');
-    submarinePreview.classList.add('submarine-preview');
-    submarinePreview.classList.add('submarine');
-    optionsCont.append(submarinePreview);
-
-    const destroyerPreview = document.createElement('div');
-    destroyerPreview.classList.add('destroyer-preview');
-    destroyerPreview.classList.add('destroyer');
-    optionsCont.append(destroyerPreview);
-
-    return pageCont;
+// Option choosing
+let angle = 0;
+function flip () {
+    const optionsShips = Array.from(optionsCont.children);
+    // if angle equals 0, return 90, otherwise return 0
+    angle = angle === 0 ? 90 : 0
+    optionsShips.forEach(optionsShip => optionsShip.style.transform = `rotate(${angle}deg)`);
 }
 
-function shipPreviewGen(name){
-    const shipPreview = document.createElement('div');
-    shipPreview.setAttribute('id', `${name}-preview`);
-    optionsCont.append(shipPreview);
+flipButton.addEventListener('click', flip);
+
+// Creating Boards
+const width = 10;
+
+function createBoard(){
+    const gameBoardCont = document.createElement('div');
+    gameBoardCont.classList.add('game-board');
+    gameBoardCont.style.backgroundColor = 'pink';
+
+    gameboardsCont.append(gameBoardCont);
 }
 
-
-function generateBoard(){
-    for (let i = 0; i < 10; i++){
-        let boardRow = document.createElement('div');
-        boardRow.classList.add('row');
-        boardRow.setAttribute('row', i);
-        function spaceCreate (){
-            for(let k = 0; k < 10; k++){
-                const space = document.createElement('div');
-                space.classList.add('column');
-                space.setAttribute('column', k);
-                boardRow.append(space);
-            }
-        }
-        spaceCreate();
-        pageCont.append(boardRow);
-    }
-}
-
-// const gameboard = createGameboard();
-// console.log(gameboard.placeShip(3, 0, 3));
-
-const page = generatePage();
-content.append(page);
-
+createBoard();
+createBoard();
